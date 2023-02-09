47d6f4740a4b71210a35a3d472cf60830093e593
lcsfaccio <104501676+lcsfaccio@users.noreply.github.com>
2023-02-09 00:04:19 -0300

    Update session_state.py

--- session_state.py
+++ session_state.py
@@ -1,16 +1,16 @@
 import streamlit as st
-import os
-import dotenv
+#import os
+from dotenv import load_dotenv
 import streamlit_google_oauth as oauth
 
 load_dotenv()
-os.environ["GOOGLE_CLIENT_ID"] ="186940062697-acgucff8i09gu24f5akkjnassgt7uceh.apps.googleusercontent.com"
-os.environ["GOOGLE_CLIENT_SECRET"] ="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"
-os.environ["GOOGLE_REDIRECT_URI"] ="http://localhost:8501"
+# os.environ["GOOGLE_CLIENT_ID"] ="186940062697-acgucff8i09gu24f5akkjnassgt7uceh.apps.googleusercontent.com"
+# os.environ["GOOGLE_CLIENT_SECRET"] ="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"
+# os.environ["GOOGLE_REDIRECT_URI"] ="http://localhost:8501"
 
-client_id = os.environ["GOOGLE_CLIENT_ID"]
-client_secret = os.environ["GOOGLE_CLIENT_SECRET"]
-redirect_uri = os.environ["GOOGLE_REDIRECT_URI"]
+client_id = st.secrets["GOOGLE_CLIENT_ID"]
+client_secret = st.secrets["GOOGLE_CLIENT_SECRET"]
+redirect_uri = st.secrets["GOOGLE_REDIRECT_URI"]
 
 
 if __name__ == "__main__":
