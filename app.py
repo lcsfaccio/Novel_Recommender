aa48a5fed4f9a93b698ec15754d39718d824f2fa
lcsfaccio <104501676+lcsfaccio@users.noreply.github.com>
2023-02-09 00:05:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+#------------------------------------------------------------------------------------------
+import asyncio
+from session_state import *
+login_info = oauth.login(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        login_button_text="Continue with Google",
+        logout_button_text="Logout",
+    )
 
+if login_info:
+        user_id, user_email = login_info
+        st.write(f"Welcome {user_email}")
+else:
+        st.write("Please login")
+#------------------------------------------------------------------------------------------
 df = pd.read_csv("algoritmo - Base.csv")
 
 # filtros para a tabela
